6975cb5879b62503fcc94b2b86661588f0c698a1
Dichill <dichilltomarong@yahoo.com>
2025-03-28 23:48:57 -0700

    fixed shop ui and added model for seed and bugs

---
+++ Assets/Scripts/Model/BugModel.cs
@@ -0,0 +1,19 @@
+public class BugModel
+{
+    public string name;
+    public int price;
+    public int amount;
+    public BugType bugType;
+    public Rarity rarity;
+}
+
+public enum BugType
+{
+    Ant
+}
+
+public enum Rarity
+{
+    Basic,
+    Rare
+}
---
+++ Assets/Scripts/Model/SeedModel.cs
@@ -0,0 +1,12 @@
+public class SeedModel
+{
+    public string name;
+    public int price;
+    public int amount;
+    public SeedType seedType;
+}
+
+public enum SeedType
+{
+    Fern
+}
