fc938f7d69ff9a7821e7347227695bacfadf85b4
Dichill <dichilltomarong@yahoo.com>
2025-03-29 23:42:31 -0700

    error fixes

--- Assets/Scripts/Plant.cs
+++ Assets/Scripts/Plant.cs
@@ -6,32 +6,38 @@ public class Plant : MonoBehaviour, IInteractable
 {
     [SerializeField] private GameObject model; // assign in editor
     private enum AgeState { Sprout, Mature }
-    private AgeState currentAge; 
-    public PlantData currentPlantType;
+    private AgeState currentAge;
+    // public PlantData currentPlantType;
     private BoxCollider boxCollider; // toggle collider for interaction
 
-    void Start() {
+    void Start()
+    {
         boxCollider = gameObject.GetComponent<BoxCollider>();
         boxCollider.enabled = false;
         StartCoroutine(BugSpawnTimer());
     }
 
-    private void Update() {
-        SetModel(); 
-        
+    private void Update()
+    {
+        SetModel();
+
         //plant spawning debug keys
-        if (Input.GetKeyDown(KeyCode.L)) {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
             currentAge = AgeState.Mature;
             Debug.Log(currentAge);
         }
-        if (Input.GetKeyDown(KeyCode.K)) {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
             currentAge = AgeState.Sprout;
             Debug.Log(currentAge);
         }
     }
 
-    private void SetModel() {
-        if (currentAge == AgeState.Sprout) {
+    private void SetModel()
+    {
+        if (currentAge == AgeState.Sprout)
+        {
             model.SetActive(false);
             boxCollider.enabled = false;
             return;
@@ -41,34 +47,39 @@ public class Plant : MonoBehaviour, IInteractable
     }
 
 
-    
-#region IInteractable
+
+    #region IInteractable
     [SerializeField] private string _prompt;
     public string InteractionPrompt => _prompt;
-    public bool Interact(PlayerController playerController) {
+    public bool Interact(PlayerController playerController)
+    {
         Debug.Log("Plant Interacted!");
         TakeBug();
         return true;
     }
-#endregion
+    #endregion
 
-#region Bug Handling
+    #region Bug Handling
     private bool hasBug = false;
     //make currentBug variable
     [SerializeField] private float bugSpawnDelay;
     /// <summary>
     /// A timer that attempts to spawn a bug if possible after a certain amount of time. Runs as long as this object exists.
     /// </summary>
-    private IEnumerator BugSpawnTimer() {
-        while (true) {
+    private IEnumerator BugSpawnTimer()
+    {
+        while (true)
+        {
             yield return new WaitForSeconds(bugSpawnDelay);
-            if(!hasBug) SpawnNewBug();
+            if (!hasBug) SpawnNewBug();
         }
     }
-    
+
     //TODO
-    private void TakeBug() { 
-        if (hasBug) {
+    private void TakeBug()
+    {
+        if (hasBug)
+        {
             Debug.Log("No bug to take :(");
             return;
         }
@@ -78,21 +89,23 @@ public class Plant : MonoBehaviour, IInteractable
     }
 
     //TODO
-    private void SpawnNewBug() {
-        if (!hasBug) {
+    private void SpawnNewBug()
+    {
+        if (!hasBug)
+        {
             //insert spawn bug functionality
-            
+
             hasBug = true;
             Debug.Log("Bug spawned");
             return;
         }
     }
-#endregion
+    #endregion
 
-#region UI
-// promptUI
-#endregion
-  
+    #region UI
+    // promptUI
+    #endregion
+
 }
 
 
