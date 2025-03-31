00d7730bb12071d44f011e345a7117a53f0dd43a
MattOrbita <mattorbita246@gmail.com>
2025-03-30 17:00:40 -0700

    Remove unused, error-causing code in PlayerController, make first build

--- Assets/Scripts/YN/PlayerController.cs
+++ Assets/Scripts/YN/PlayerController.cs
@@ -3,7 +3,6 @@ using System.Threading;
 using System.Threading.Tasks;
 using Unity.VisualScripting;
 using UnityEditor;
-using UnityEditor.Search;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
